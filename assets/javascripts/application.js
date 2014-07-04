ab245d14949b8e3b610b6618eca41b4c311d44a5
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-07-04 09:55:39 -0300

    main header

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,2 +1,33 @@
 jQuery(document).ready(function($) {
+  new ContactBoxManager($('header.main a.contact'), $('header.main .contact-box'))
 });
+
+var ContactBoxManager = function($openBnt, $box){
+  var _this = this;
+  this.$openBnt  = $openBnt;
+  this.$box      = $box;
+
+  this.startup = function(){
+    $('header.main').on('click', 'a.contact', function(event) {
+      event.preventDefault();
+      
+      _this.open();
+    });
+
+    $box.on('click', 'a.close', function(event) {
+      event.preventDefault();
+      
+      _this.close();
+    });
+  },
+
+  this.open = function(){
+    this.$box.addClass('openned');
+  },
+
+  this.close = function(){
+    this.$box.removeClass('openned');
+  },
+
+  this.startup();
+}
